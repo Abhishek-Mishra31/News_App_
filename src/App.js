6daa29da3fc3f543859c8dc596ee5a5d0b33e66b
Abhishekbelaganj0609@gmail.com <Abhishekbelaganj0609@gmail.com>
2024-02-20 21:07:36 +0530

    News_App_Uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NewsItem from "./Components/NewsItem";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  ApiKey = process.env.REACT_APP_MY_API_KEY;
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <NewsItem
+                  key="general"
+                  apikey={this.ApiKey}
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Business"
+              element={
+                <NewsItem
+                  key="Business"
+                  apikey={this.ApiKey}
+                  category="Business"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Entertainment"
+              element={
+                <NewsItem
+                  key="Entertainment"
+                  apikey={this.ApiKey}
+                  category="Entertainment"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Health"
+              element={
+                <NewsItem key="Health" apikey={this.ApiKey} category="Health" />
+              }
+            />
+            <Route
+              exact
+              path="/science"
+              element={
+                <NewsItem
+                  key="science"
+                  apikey={this.ApiKey}
+                  category="Science"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Sports"
+              element={
+                <NewsItem key="Sports" apikey={this.ApiKey} category="Sports" />
+              }
+            />
+
+            <Route
+              exact
+              path="/Technology"
+              element={
+                <NewsItem
+                  key="Technology"
+                  apikey={this.ApiKey}
+                  category="Technology"
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
